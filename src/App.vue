db51516de6a52b0c70f0b26a5b0f7424dc1b159c
wojiaohuiyu <919072881@qq.com>
2019-04-29 12:34:42 +0800

    第一次提交项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <TopNav />
+  </div>
+</template>
+
+<script>
+import TopNav from './TopNav.vue'
+
+export default {
+  components: {
+    TopNav
+  }
+}
+</script>
+
+<style>
+.hello {
+ color: red;
+}
+</style>
